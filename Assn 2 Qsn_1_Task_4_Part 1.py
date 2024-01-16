cc2506cb1170e38401660f8647ad950e0763abbb
SharifMuktadirHC <156582814+SharifMuktadirHC@users.noreply.github.com>
2024-01-16 23:16:07 +1100

    Add files via upload

---
+++ Assn 2 Qsn_1_Task_4_Part 1.py
@@ -0,0 +1,12 @@
+# A 2 Q 1 Task 4 part 1: Extract the ‘diseases’, and ‘drugs’ entities in the ‘extracted_text.txt file’ separately using ‘en_core_sci_sm’.
+
+import spacy
+import scispacy
+nlp = spacy.load('en_core_sci_sm')
+with open('extracted_text.txt', 'r') as f:
+    text = f.read()
+doc = nlp(text)
+diseases = [ent.text for ent in doc.ents if ent.label_ == 'DISEASE']
+drugs = [ent.text for ent in doc.ents if ent.label_ == 'DRUG']
+print(diseases)
+print(drugs)
